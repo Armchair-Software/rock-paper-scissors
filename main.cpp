7a2ba86cfb19ffc73d25b23dedf2fc1103a907b6
SlowRiot <slowriot@voxelstorm.com>
2023-04-05 16:52:36 +0100

    customisable number of rounds

--- main.cpp
+++ main.cpp
@@ -26,6 +26,10 @@ static void loop_opponent_move(void *data);
 struct game_state {
   render::window &window;
   game_logic::game_logic_base &logic;
+  unsigned int rounds_to_play = 5;
+  unsigned int rounds_played = 0;
+  unsigned int wins = 0;
+  unsigned int draws = 0;
 };
 
 [[noreturn]] auto main()->int {                                                 // noreturn here is not standards-compliant, but is appropriate for emscripten with a main loop
@@ -66,15 +70,20 @@ void loop_select_difficulty(void *data) {
 
   bool move_to_main_loop{false};
 
-  ImGui::SetNextWindowSize(ImVec2(200, -FLT_MIN), ImGuiCond_FirstUseEver);
-  ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f)); // set next window position in the centre of the frame, use pivot=(0.5f,0.5f) to center on given point, etc.
-  if(ImGui::Begin("Select Difficulty", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse)) {
-    // TODO: number of rounds to play
+  ImGui::SetNextWindowSize(vec2f{0, -FLT_MIN}, ImGuiCond_FirstUseEver);
+  ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, vec2f{0.5f, 0.5f}); // set next window position in the centre of the frame, use pivot=(0.5f,0.5f) to center on given point, etc.
+  if(ImGui::Begin("Start game", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse)) {
+    ImGui::TextUnformatted("Rounds to play");
+    ImGui::SetNextItemWidth(-FLT_MIN);                                          // stretch next input to full width
+    if(ImGui::InputInt("##rounds", reinterpret_cast<int*>(&state.rounds_to_play), 1, 5)) { // input for how many rounds to play
+      state.rounds_to_play = std::clamp(state.rounds_to_play, 1u, 100u);        // keep the numbers within a reasonable range
+    }
 
+    ImGui::TextUnformatted("Difficulty");
     magic_enum::enum_for_each<game_logic::difficulty_type>([&](game_logic::difficulty_type difficulty) {
       std::string difficulty_name{magic_enum::enum_name(difficulty)};           // derive difficulty name from enum name
       difficulty_name[0] = static_cast<char>(std::toupper(difficulty_name[0])); // capitalise first letter of the difficulty name
-      if(ImGui::Button(difficulty_name.c_str())) {                              // difficulty select button
+      if(ImGui::Button(difficulty_name.c_str(), vec2f{200, 40})) {              // difficulty select button
         state.logic.set_difficulty(difficulty);                                 // set the difficulty in the game logic engine
         move_to_main_loop = true;                                               // proceed to the main loop
       }
@@ -108,12 +117,13 @@ void loop_player_move(void *data) {
   ImGui_ImplGlfw_NewFrame();
   ImGui::NewFrame();
 
-  ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, ImVec2(0.5f, 0.5f)); // set next window position in the centre of the frame, use pivot=(0.5f,0.5f) to center on given point, etc.
+  ImGui::SetNextWindowSize(vec2f{0, -FLT_MIN}, ImGuiCond_FirstUseEver);
+  ImGui::SetNextWindowPos(ImGui::GetMainViewport()->GetCenter(), ImGuiCond_Appearing, vec2f{0.5f, 0.5f}); // set next window position in the centre of the frame, use pivot=(0.5f,0.5f) to center on given point, etc.
   if(ImGui::Begin("Choose your move", nullptr, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse)) {
     magic_enum::enum_for_each<game_logic::move_type>([&](game_logic::move_type move) {
       std::string move_name{magic_enum::enum_name(move)};                       // derive move name from enum name
       move_name[0] = static_cast<char>(std::toupper(move_name[0]));             // capitalise first letter of the name
-      if(ImGui::Button(move_name.c_str())) {                                    // select move button
+      if(ImGui::Button(move_name.c_str(), vec2f{100, 40})) {                    // select move button
         state.logic.process();                                                  // calculate the next move
         state.logic.player_move(move);                                          // declare the player move
         next_loop = next_loop_type::opponent_move;                              // opponent's turn
@@ -123,7 +133,6 @@ void loop_player_move(void *data) {
       }
       ImGui::SameLine();
     });
-    ImGui::NewLine();
   }
   ImGui::End();
 
@@ -133,8 +142,23 @@ void loop_player_move(void *data) {
 
   switch(next_loop) {
   case next_loop_type::opponent_move:
-    emscripten_cancel_main_loop();
-    emscripten_set_main_loop_arg(&loop_opponent_move, &state, 0, true);
+    {
+      ++state.rounds_played;
+      auto verdict{state.logic.get_last_verdict()};
+      switch(verdict) {
+      case game_logic::verdict_type::win:
+        ++state.wins;
+        break;
+      case game_logic::verdict_type::draw:
+        ++state.draws;
+        break;
+      case game_logic::verdict_type::lose:
+        // losses are calculated automatically from total, wins and draws
+        break;
+      }
+      emscripten_cancel_main_loop();
+      emscripten_set_main_loop_arg(&loop_opponent_move, &state, 0, true);
+    }
     break;
   case next_loop_type::select_difficulty:
     emscripten_cancel_main_loop();
@@ -164,11 +188,20 @@ void loop_opponent_move(void *data) {
     ImGui::TextUnformatted(("Your opponent's move: " + std::string{magic_enum::enum_name(opponent_move)} + "!").c_str());
     auto verdict{state.logic.get_last_verdict()};
     ImGui::TextUnformatted(("Result: " + std::string{magic_enum::enum_name(verdict)}).c_str());
+    std::stringstream totalss;
+    auto const losses{state.rounds_played - state.wins - state.draws};
+    totalss << "Total: " << state.rounds_played << " rounds with "
+            << state.wins  << " wins ("   << (state.wins  * 100) / state.rounds_played << "%), "
+            << state.draws << " draws ("  << (state.draws * 100) / state.rounds_played << "%), "
+            << losses      << " losses (" << (losses      * 100) / state.rounds_played << "%)";
+    ImGui::TextUnformatted(totalss.str().c_str());
 
-    if(ImGui::Button("Continue")) {
-      next_loop = next_loop_type::player_move;                                  // proceed to the player's turn
+    if(state.rounds_played < state.rounds_to_play) {                            // only show the next round button if we have more rounds to play
+      if(ImGui::Button("Next round")) {
+        next_loop = next_loop_type::player_move;                                // proceed to the player's turn
+      }
     }
-    if(ImGui::Button("Change difficulty")) {
+    if(ImGui::Button("End game")) {
       next_loop = next_loop_type::select_difficulty;                            // proceed to the difficulty select loop
     }
   }
@@ -184,6 +217,9 @@ void loop_opponent_move(void *data) {
     emscripten_set_main_loop_arg(&loop_player_move, &state, 0, true);
     break;
   case next_loop_type::select_difficulty:
+    state.rounds_played = 0;
+    state.wins = 0;
+    state.draws = 0;
     emscripten_cancel_main_loop();
     emscripten_set_main_loop_arg(&loop_select_difficulty, &state, 0, true);
     break;
--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 1;
 	static const long MINOR  = 1;
-	static const long BUILD  = 173;
-	static const long REVISION  = 994;
+	static const long BUILD  = 197;
+	static const long REVISION  = 1113;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 183;
-	#define RC_FILEVERSION 1,1,173,994
-	#define RC_FILEVERSION_STRING "1, 1, 173, 994\0"
-	static const char FULLVERSION_STRING [] = "1.1.173.994";
+	static const long BUILDS_COUNT  = 209;
+	#define RC_FILEVERSION 1,1,197,1113
+	#define RC_FILEVERSION_STRING "1, 1, 197, 1113\0"
+	static const char FULLVERSION_STRING [] = "1.1.197.1113";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 73;
+	static const long BUILD_HISTORY  = 97;
 	
 
 }
