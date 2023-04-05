111d25d75567abda4af806c051467de2b411c3de
SlowRiot <slowriot@voxelstorm.com>
2023-04-05 14:59:26 +0100

    add offline game logic, implementing pluggable local strategies by difficulty

---
+++ game_logic/offline.cpp
@@ -0,0 +1,76 @@
+#include "offline.h"
+#include <iostream>
+#include <iomanip>
+#include <magic_enum.hpp>
+#include "strategy/random.h"
+
+namespace game_logic {
+
+void offline::set_difficulty(difficulty_type new_difficulty) {
+  /// Update the game difficulty, and initialise a strategy based on it
+  game_logic_base::set_difficulty(new_difficulty);                              // base difficulty change effects should apply first
+
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic error "-Wswitch"                                       // enforce exhaustive switch here
+  switch(difficulty) {
+  #pragma GCC diagnostic pop
+  case difficulty_type::easy:
+    strategy = std::make_unique<strategy::random>();
+    // TODO: implement easy strategy
+    break;
+  case difficulty_type::medium:
+    strategy = std::make_unique<strategy::random>();
+    break;
+  case difficulty_type::hard:
+    strategy = std::make_unique<strategy::random>();
+    // TODO: implement hard strategy
+    break;
+  }                                                                             // no default case, to enforce exhaustive switch
+}
+
+std::string offline::difficulty_description(difficulty_type target_difficulty) const {
+  /// Difficulty descriptions specific to offline local AI play
+  #pragma GCC diagnostic push
+  #pragma GCC diagnostic error "-Wswitch"                                       // enforce exhaustive switch here
+  switch(target_difficulty) {
+  #pragma GCC diagnostic pop
+  case difficulty_type::easy:
+    return "The computer will use strategy to try to lose.";
+  case difficulty_type::medium:
+    return "The computer will play at random.";
+  case difficulty_type::hard:
+    return "The computer will use strategy to try to win.";
+  }                                                                             // no default case, to enforce exhaustive switch
+}
+
+void offline::process() {
+  /// Determine and report the opponent's move
+  assert(strategy);                                                             // used in lieu of exceptions, as we have no exception support
+
+  opponent_last_move = strategy->get_next_move();                               // ask the current strategy to generate the next move and cache the result
+  std::cout << "Offline logic: Opponent move: " << std::quoted(magic_enum::enum_name(opponent_last_move)) << std::endl;
+}
+
+void offline::player_move(move_type move) {
+  /// Declare a player move
+  assert(strategy);                                                             // used in lieu of exceptions, as we have no exception support
+
+  std::cout << "Offline logic: Player move: " << std::quoted(magic_enum::enum_name(move)) << std::endl;
+  player_last_move = move;
+  strategy->declare_last_player_move(move);                                     // only inform the strategy about the move the player made after it tells us its move, to prevent possibility of cheating strategies
+
+  last_verdict = verdict(player_last_move, opponent_last_move);                 // determine and update the verdict
+  std::cout << "Offline logic: Verdict: " << std::quoted(magic_enum::enum_name(last_verdict)) << std::endl;
+}
+
+move_type offline::get_last_opponent_move() const {
+  /// Report the opponent's last move
+  return opponent_last_move;
+}
+
+verdict_type offline::get_last_verdict() const {
+  /// Report the verdict of the last game
+  return last_verdict;
+}
+
+}
---
+++ game_logic/offline.h
@@ -0,0 +1,30 @@
+#ifndef GAME_LOGIC_OFFLINE_H_INCLUDED
+#define GAME_LOGIC_OFFLINE_H_INCLUDED
+
+#include "game_logic_base.h"
+#include "strategy/strategy_base.h"
+
+namespace game_logic {
+
+class offline : public game_logic_base {
+  /// Game logic: local AI for offline play
+  std::unique_ptr<strategy::strategy_base> strategy;                            // the strategy we will use - dynamically chosen based on requested difficulty
+
+  move_type player_last_move;                                                   // the player's last move
+  move_type opponent_last_move;                                                 // cache of the opponent's last move
+  verdict_type last_verdict;                                                    // cache of the last game outcome (win, draw, lose)
+
+public:
+  virtual void set_difficulty(difficulty_type new_difficulty) override final;
+
+  virtual std::string difficulty_description(difficulty_type difficulty) const override final;
+
+  virtual void process() override final;
+  virtual void player_move(move_type move) override final;
+  virtual move_type get_last_opponent_move() const override final;
+  virtual verdict_type get_last_verdict() const override final;
+};
+
+}
+
+#endif // GAME_LOGIC_OFFLINE_H_INCLUDED
