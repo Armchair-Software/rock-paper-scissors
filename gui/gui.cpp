8087c0976fcfba33f5d863db233bcb7e116ee6f4
SlowRiot <slowriot@voxelstorm.com>
2023-04-05 21:51:35 +0100

    tidy up colour definitions in gui

--- gui/gui.cpp
+++ gui/gui.cpp
@@ -16,7 +16,7 @@ void init(render::window &window) {
   imgui_io.IniFilename = nullptr;                                               // disable saving settings to disk
   imgui_io.ConfigFlags |= ImGuiConfigFlags_NoMouseCursorChange;                 // don't attempt to change mouse cursors
   imgui_io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;                   // enable keyboard controls
-  //imgui_io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;                    // enable gamepad controls
+  imgui_io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;                    // enable gamepad controls
 
   ImGui::StyleColorsDark();                                                     // imgui style
   ImGuiStyle& style = ImGui::GetStyle();
@@ -48,80 +48,46 @@ void init(render::window &window) {
     return vec4f(colour_rgb.r, colour_rgb.g, colour_rgb.b, alpha);
   };
   ImVec4 *colors{ImGui::GetStyle().Colors};
-
-  // lightness 15
-  colors[ImGuiCol_FrameBgHovered]         = get_float_colour(15, 0.40f);
-  colors[ImGuiCol_FrameBgActive]          = get_float_colour(15, 0.67f);
-  colors[ImGuiCol_CheckMark]              = get_float_colour(15);
-  colors[ImGuiCol_SliderGrabActive]       = get_float_colour(15);
-  colors[ImGuiCol_Button]                 = get_float_colour(15, 0.40f);
-  colors[ImGuiCol_ButtonHovered]          = get_float_colour(15);
-  colors[ImGuiCol_Header]                 = get_float_colour(15, 0.31f);
-  colors[ImGuiCol_HeaderHovered]          = get_float_colour(15, 0.80f);
-  colors[ImGuiCol_HeaderActive]           = get_float_colour(15);
-  colors[ImGuiCol_ResizeGrip]             = get_float_colour(15, 0.20f);
-  colors[ImGuiCol_ResizeGripHovered]      = get_float_colour(15, 0.67f);
-  colors[ImGuiCol_ResizeGripActive]       = get_float_colour(15, 0.95f);
-  colors[ImGuiCol_TabHovered]             = get_float_colour(15, 0.80f);
-  colors[ImGuiCol_DockingPreview]         = get_float_colour(15, 0.70f);
-  colors[ImGuiCol_TextSelectedBg]         = get_float_colour(15, 0.35f);
-  colors[ImGuiCol_NavHighlight]           = get_float_colour(15);
-
-  // lightness 14
-  colors[ImGuiCol_SliderGrab]             = get_float_colour(14);
-
-  // lightness 13
-  colors[ImGuiCol_ButtonActive]           = get_float_colour(13);
-
-  // lightness 12
-  colors[ImGuiCol_ScrollbarGrabActive]    = get_float_colour(12);
-
-  // lightness 11
-  colors[ImGuiCol_Border]                 = get_float_colour(11, 0.50f);
-  colors[ImGuiCol_Separator]              = get_float_colour(11, 0.50f);
-
-  // lightness 10
-  colors[ImGuiCol_ScrollbarGrabHovered]   = get_float_colour(10);
-  colors[ImGuiCol_SeparatorHovered]       = get_float_colour(10, 0.78f);
-  colors[ImGuiCol_SeparatorActive]        = get_float_colour(10);
-  colors[ImGuiCol_TabActive]              = get_float_colour(10);
-
-  // lightness 9
-  colors[ImGuiCol_Tab]                    = get_float_colour(9, 0.86f);
-
-  // lightness 8
-  colors[ImGuiCol_TableBorderStrong]      = get_float_colour(8);
-  colors[ImGuiCol_FrameBg]                = get_float_colour(8, 0.54f);
-  colors[ImGuiCol_TitleBgActive]          = get_float_colour(8);
-
-  // lightness 7
-  colors[ImGuiCol_ScrollbarGrab]          = get_float_colour(7);
-  colors[ImGuiCol_TabUnfocusedActive]     = get_float_colour(7);
-
-  // lightness 6
-  colors[ImGuiCol_TableBorderLight]       = get_float_colour(6);
-
-  // lightness 5
-  colors[ImGuiCol_DockingEmptyBg]         = get_float_colour(5);
-  colors[ImGuiCol_TableHeaderBg]          = get_float_colour(5);
-
-  // lightness 4
-  // no overrides
-
-  // lightness 3
-  colors[ImGuiCol_MenuBarBg]              = get_float_colour(3);
-  colors[ImGuiCol_TabUnfocused]           = get_float_colour(3);
-
-  // lightness 2
-  colors[ImGuiCol_PopupBg]                = get_float_colour(2, 0.94f);
-  colors[ImGuiCol_WindowBg]               = get_float_colour(2, 0.90f);
-
-  // lightness 1
-  colors[ImGuiCol_TitleBg]                = get_float_colour(1);
-  colors[ImGuiCol_ScrollbarBg]            = get_float_colour(1, 0.53f);
-
-  // lightness 0
-  // no overrides
+  colors[ImGuiCol_FrameBgHovered]       = get_float_colour(15, 0.40f);
+  colors[ImGuiCol_FrameBgActive]        = get_float_colour(15, 0.67f);
+  colors[ImGuiCol_CheckMark]            = get_float_colour(15);
+  colors[ImGuiCol_SliderGrabActive]     = get_float_colour(15);
+  colors[ImGuiCol_Button]               = get_float_colour(15, 0.40f);
+  colors[ImGuiCol_ButtonHovered]        = get_float_colour(15);
+  colors[ImGuiCol_Header]               = get_float_colour(15, 0.31f);
+  colors[ImGuiCol_HeaderHovered]        = get_float_colour(15, 0.80f);
+  colors[ImGuiCol_HeaderActive]         = get_float_colour(15);
+  colors[ImGuiCol_ResizeGrip]           = get_float_colour(15, 0.20f);
+  colors[ImGuiCol_ResizeGripHovered]    = get_float_colour(15, 0.67f);
+  colors[ImGuiCol_ResizeGripActive]     = get_float_colour(15, 0.95f);
+  colors[ImGuiCol_TabHovered]           = get_float_colour(15, 0.80f);
+  colors[ImGuiCol_DockingPreview]       = get_float_colour(15, 0.70f);
+  colors[ImGuiCol_TextSelectedBg]       = get_float_colour(15, 0.35f);
+  colors[ImGuiCol_NavHighlight]         = get_float_colour(15);
+  colors[ImGuiCol_SliderGrab]           = get_float_colour(14);
+  colors[ImGuiCol_ButtonActive]         = get_float_colour(13);
+  colors[ImGuiCol_ScrollbarGrabActive]  = get_float_colour(12);
+  colors[ImGuiCol_Border]               = get_float_colour(11, 0.50f);
+  colors[ImGuiCol_Separator]            = get_float_colour(11, 0.50f);
+  colors[ImGuiCol_ScrollbarGrabHovered] = get_float_colour(10);
+  colors[ImGuiCol_SeparatorHovered]     = get_float_colour(10, 0.78f);
+  colors[ImGuiCol_SeparatorActive]      = get_float_colour(10);
+  colors[ImGuiCol_TabActive]            = get_float_colour(10);
+  colors[ImGuiCol_Tab]                  = get_float_colour(9, 0.86f);
+  colors[ImGuiCol_TableBorderStrong]    = get_float_colour(8);
+  colors[ImGuiCol_FrameBg]              = get_float_colour(8, 0.54f);
+  colors[ImGuiCol_TitleBgActive]        = get_float_colour(8);
+  colors[ImGuiCol_ScrollbarGrab]        = get_float_colour(7);
+  colors[ImGuiCol_TabUnfocusedActive]   = get_float_colour(7);
+  colors[ImGuiCol_TableBorderLight]     = get_float_colour(6);
+  colors[ImGuiCol_DockingEmptyBg]       = get_float_colour(5);
+  colors[ImGuiCol_TableHeaderBg]        = get_float_colour(5);
+  colors[ImGuiCol_MenuBarBg]            = get_float_colour(3);
+  colors[ImGuiCol_TabUnfocused]         = get_float_colour(3);
+  colors[ImGuiCol_PopupBg]              = get_float_colour(2, 0.94f);
+  colors[ImGuiCol_WindowBg]             = get_float_colour(2, 0.90f);
+  colors[ImGuiCol_TitleBg]              = get_float_colour(1);
+  colors[ImGuiCol_ScrollbarBg]          = get_float_colour(1, 0.53f);
 
   ImGui_ImplGlfw_InitForOpenGL(window.glfw_window, true);                       // set up imgui backends
   ImGui_ImplOpenGL3_Init("#version 300 es");                                    // version 300 es for OpenGL ES 3.0 applies to WebGL 2.0
