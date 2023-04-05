be58e8aba9caf615f1a86e9260370c891cb22bdc
SlowRiot <slowriot@voxelstorm.com>
2023-04-05 14:38:45 +0100

    add game difficulty types

---
+++ game_logic/difficulty.h
@@ -0,0 +1,15 @@
+#ifndef GAME_LOGIC_DIFFICULTY_H_INCLUDED
+#define GAME_LOGIC_DIFFICULTY_H_INCLUDED
+
+namespace game_logic {
+
+enum class difficulty_type : unsigned int {
+  /// Available game difficulty levels
+  easy,
+  medium,
+  hard,
+};
+
+}
+
+#endif // GAME_LOGIC_DIFFICULTY_H_INCLUDED
